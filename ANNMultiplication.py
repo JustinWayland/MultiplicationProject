5cc8d98aafa658fbb70b8a6f493b23402fd89f5d
JustinWayland <justintwayland+github@gmail.com>
2016-08-04 14:41:39 -0400

    Changed fitness function to better fit what we are aiming for.

--- ANNMultiplication.py
+++ ANNMultiplication.py
@@ -1,3 +1,6 @@
+# Currently in desperate need of a revamp
+#  - Need better error function for judging fitness; sheer magnitude of wrong answers vs. right answers is very large
+
 from __future__ import division
 from deps.peas.networks.rnn import NeuralNetwork
 from deps.peas.methods.neat import NEATPopulation, NEATGenotype
@@ -27,14 +30,31 @@ def pack(a):
         b += b
     return s
 
+def fitnessFunction(output,target):
+    assert len(output) == len(target)
+    indices = [i for i in range(len(target))]
+    indices.reverse()
+    s = 0
+    for o in indices:
+        if output[o] > 0.5:
+            bit = 1
+        else:
+            bit = 0
+        if bit == target[0]:
+            s += 1
+        else:
+            return s
+    return s
+
 class MultiplicationTask(object):
     def __init__(self, run_length, bit_sizes, accuracy_threshold, runs_file):
         #Create fixed runs
         inputs = [(random.getrandbits(bit_sizes), random.getrandbits(bit_sizes)) for i in range(run_length)]
         # self.inputs = [tuple(chain([unravel(n, bit_sizes) for n in item])) for item in inputs]
-        self.inputs = [tuple(unravel(x, bit_sizes) + unravel(y, bit_sizes)) for x, y in inputs]
-        self.outputs = [a*b for a, b in inputs]
-        for t, product in zip(inputs, self.outputs):
+        self.inputs = np.array([tuple(unravel(x, bit_sizes) + unravel(y, bit_sizes)) for x, y in inputs], dtype=float)
+        self.outputs = [unravel(a*b,bit_sizes*2) for a, b in inputs]
+        self.pairs = zip(self.inputs, self.outputs)
+        for t, product in zip(inputs, [a*b for a, b in inputs]):
             x, y = t
             runs_file.write(','.join(['fixed', str(x), str(y), str(product)]))
             runs_file.write('\n')
@@ -47,30 +67,16 @@ class MultiplicationTask(object):
         if not isinstance(network,NeuralNetwork):
             network = NeuralNetwork(network)
         network.make_feedforward()
-        if not network.node_types[-1](-1000) < -0.95:
-            raise Exception("Network should be able to output value of -1, e.g. using a tanh node.")
-        #Create random runs (so as to prevent overfitting)
-        new_inputs = [(random.getrandbits(self.bit_sizes), random.getrandbits(self.bit_sizes)) for i in range(self.run_length)]
-        new_outputs = [a*b for a, b in new_inputs]
-        for t, product in zip(new_inputs, new_outputs):
-            x, y = t
-            self.runs_file.write(','.join(['random', str(x), str(y), str(product)]))
-            self.runs_file.write('\n')
-        inputs = self.inputs + [tuple(unravel(x, self.bit_sizes) + unravel(y, self.bit_sizes)) for x, y in new_inputs]
-        outputs = self.outputs + new_outputs
-        pairs = zip(np.array(inputs, dtype=float), outputs)
-        random.shuffle(pairs)
-        err = 0
-        for (i, target) in pairs:
+        score = 0
+        for (i, target) in self.pairs:
             output = network.feed(i)
-            output = pack(output[-self.bit_sizes*2:])
-            err += abs(target - output)/target
-        score = 1 - (err/len(pairs))
-        return {'fitness': score}
+            output = output[-self.bit_sizes*2:]
+            score += fitnessFunction(output,target)
+        return {'fitness': score/len(self.pairs)}
 
     def solve(self, network):
         """ Defines if a network has solved the problem. Size matters more than accuracy, but error should not exceed a certain margin. """
-        return int(self.evaluate(network) >= self.min_accuracy)
+        return self.evaluate(network)['fitness'] >= min_accuracy
 
 TaskParams = namedtuple('TaskParams', ['bitsize', 'weight_range', 'neuron_type', 'popsize', 'run_length', 'accuracy_threshold', 'generations', 'ann_file', 'runs_file'])
 
@@ -88,8 +94,22 @@ def convertParameters(bitsize, weight_min, weight_max, neuron_type, popsize, run
 with open("example.csv", "r") as ps:
     for p in starmap(convertParameters, csv.reader(ps)):
         def genotype():
-            return NEATGenotype(inputs=p.bitsize*2, outputs=p.bitsize*2, weight_range=p.weight_range, types=[p.neuron_type])
-        with open(p.runs_file, "a") as r:
+            i = 0
+            inputs = [r+1 for r in range(p.bitsize*2)]
+            outputs = [p.bitsize*2+a for a in range(p.bitsize*2)]
+            l = random.randrange(0, p.bitsize*2+1)
+            assert l <= p.bitsize*2
+            i = random.sample(l,inputs)
+            o = random.sample(l,outputs)
+            random.shuffle(i)
+            random.shuffle(o)
+            return NEATGenotype(inputs=p.bitsize*2,
+                                outputs=p.bitsize*2,
+                                topology=(inputs, outputs),
+                                weight_range=p.weight_range,
+                                types=[p.neuron_type],
+                                stop_when_solved=True)
+        with open(p.runs_file, "w") as r:
             r.write('type,x,y,product')
             pop = NEATPopulation(genotype, popsize=p.popsize)
             task = MultiplicationTask(p.run_length, p.bitsize, p.accuracy_threshold, r)
